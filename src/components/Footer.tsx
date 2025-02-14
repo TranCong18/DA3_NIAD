9f7a8c49799c8be93b4ddbdeb1d847ab493c8642
Tran Cong <cong@gmail.com>
2025-02-14 12:50:20 +0700

    bổ sung footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,53 @@
 export default function Footer() {
   return (
-    <footer className="bg-green-800 text-white p-6">
-      <div className="container mx-auto text-center">
-        <p>
-          © Tổng Công ty Cổ phần Bảo hiểm Ngân hàng Đầu tư và Phát triển Việt
-          Nam (BIC)
-        </p>
-        <p>
-          Địa chỉ: Tầng 11, Tòa nhà 263 Cầu Giấy, Phường Dịch Vọng, Quận Cầu
-          Giấy, Hà Nội
-        </p>
-        <p>Điện thoại: 024.22200282 | Fax: 024.22200281</p>
-        <p>
-          Website:{" "}
-          <a href="https://www.bic.vn" className="underline">
-            www.bic.vn
-          </a>
-        </p>
+    <footer className="bg-teal-800 text-white py-6">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        {/* Bên trái: Thông tin công ty */}
+        <div className="max-w-lg">
+          <p className="text-sm">
+            © Tổng Công ty Cổ phần Bảo hiểm Ngân hàng Đầu tư và Phát triển Việt
+            Nam (BIC)
+          </p>
+          <p className="text-sm">
+            Địa chỉ: Tầng 11, Tòa nhà 263 Cầu Giấy, Phường Dịch Vọng, Quận Cầu
+            Giấy, Hà Nội
+          </p>
+          <p className="text-sm">
+            Điện thoại: 024.22200282 | Fax: 024.22200281
+          </p>
+          <p className="text-sm">
+            Website:{" "}
+            <a href="https://www.bic.vn" className="underline">
+              www.bic.vn
+            </a>{" "}
+            | mybic.vn
+          </p>
+        </div>
+
+        {/* Bên phải: Hotline và Mạng xã hội */}
+        <div className="text-right">
+          <p className="text-lg font-bold">
+            HOTLINE: <span className="text-yellow-300">1900 9456</span>
+          </p>
+          <div className="flex justify-end space-x-4 mt-2">
+            <a href="#" className="hover:text-yellow-300">
+              <img
+                src="/icons/facebook.svg"
+                alt="Facebook"
+                className="w-6 h-6"
+              />
+            </a>
+            <a href="#" className="hover:text-yellow-300">
+              <img src="/icons/youtube.svg" alt="YouTube" className="w-6 h-6" />
+            </a>
+            <a href="#" className="hover:text-yellow-300">
+              <img src="/icons/zalo.svg" alt="Zalo" className="w-6 h-6" />
+            </a>
+            <a href="#" className="hover:text-yellow-300">
+              <img src="/icons/wechat.svg" alt="WeChat" className="w-6 h-6" />
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
